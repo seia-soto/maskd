782d7c0e3bd4c5f707e2377bfd585b46fa5e9733
Seia-Soto <seia@outlook.kr>
2020-03-21 03:11:24 +0900

    Update SafelySeparatedClinics.js

--- structures/reports/SafelySeparatedClinics.js
+++ structures/reports/SafelySeparatedClinics.js
@@ -1,7 +1,5 @@
 const debug = require('debug')
-const fs = require('fs')
 const iconv = require('iconv-lite')
-const path = require('path')
 
 const { cache } = require('../../config')
 const { name } = require('../../package')
@@ -11,88 +9,103 @@ const fetch = require('../fetch')
 
 class SafelySeparatedClinics {
   constructor () {
-    this.cache = {}
-
-    this.cache.directory = path.join(__dirname, '..', '..', cache.directory)
-    this.cache.updateAt = 1000 * 60 * 60
-
     this.debug = debug(name + ':SafelySeparatedClinicsReports')
+    this.updateAfter = 1000 * 60 * 60
 
-    // NOTE: Run the updater first.
+    this.debug('creating the update function chain')
+
     this.update()
-
-    setTimeout(() => {
-      this.update()
-
-      setInterval(() => {
-        this.update()
-      }, this.cache.updateAt)
-    }, (new Date().setMinutes(0, 0, 0, 0) + this.cache.updateAt) - Date.now())
   }
 
-  async update () {
-    if (!fs.existsSync(this.cache.directory)) {
-      fs.mkdirSync(this.cache.directory, {
-        recursive: true
-      })
-    }
+  getLatestStatus () {
+    return new Promise((resolve, reject) => {
+      fetch('https://www.data.go.kr/dataset/fileDownload.do?atchFileId=FILE_000000001610332&fileDetailSn=1&publicDataDetailPk=uddi:d3f3252a-1832-48fe-8c82-82947dcef08f')
+        .then(res => resolve(res.body.pipe(iconv.decodeStream('euc-kr'))))
+        .catch(error => resolve(this.getClinicStatus()))
+    })
+  }
 
-    const now = Date.now()
+  async getExistingStatus () {
+    const results = []
 
-    this.debug('updating the situation report data at ' + now)
+    const totalCount = (await knex('SafelySeparatedClinics').count('id'))[0]['count(`id`)']
 
-    const stream = fs.createWriteStream(path.join(this.cache.directory, 'SafelySeparatedClinic.' + now + '.csv'))
-    const file = await fetch.download(
-      'https://www.data.go.kr/dataset/fileDownload.do?atchFileId=FILE_000000001610332&fileDetailSn=1&publicDataDetailPk=uddi:d3f3252a-1832-48fe-8c82-82947dcef08f',
-      0,
-      stream
-    )
+    for (let i = 0; i < Math.floor(totalCount / 50) + 1; i++) {
+      const items = await knex('SafelySeparatedClinics')
+        .where('id', '>=', i * 50)
+        .andWhere('id', '<', (i * 50) + 50)
 
-    if (file.error || !file.path) {
-      this.debug('an error occured while downloading the report dataset')
-      this.debug('error: ' + (file.error || 'unknown'))
-
-      return
-    }
-
-    const report = fs.createReadStream(file.path)
-      .pipe(iconv.decodeStream('euc-kr'))
-    const data = await parse(report)
-
-    for (let i = 0; i < data.length; i++) {
-      const existingItem = await knex('SafelySeparatedClinics')
-        .where({ identify: data[i]['연번'] })
-
-      if (!existingItem.length) {
-        await knex('SafelySeparatedClinics')
-          .insert({
-            identify: data[i]['연번'] || '',
-            province: data[i]['시도'] || '',
-            city: data[i]['시군구'] || '',
-            clinicName: data[i]['기관명'] || '',
-            address: data[i]['주소'] || '',
-            requestType: data[i]['신청유형'] || '',
-            representativeContact: data[i]['전화번호'] || '',
-            availableAt: new Date(data[i]['운영가능일자'].split('.').slice(0, 3).join('-') || 0)
-          })
-      } else {
-        await knex('SafelySeparatedClinics')
-          .update({
-            province: data[i]['시도'] || '',
-            city: data[i]['시군구'] || '',
-            clinicName: data[i]['기관명'] || '',
-            address: data[i]['주소'] || '',
-            requestType: data[i]['신청유형'] || '',
-            representativeContact: data[i]['전화번호'] || '',
-            availableAt: new Date(data[i]['운영가능일자'].split('.').slice(0, 3).join('-') || 0)
-          })
-          .where({
-            identify: data[i]['연번'] || ''
-          })
+      for (let k = 0; k < items.length; k++) {
+        results.push(items[k])
       }
     }
 
+    return results
+  }
+
+  async update () {
+    const now = Date.now()
+
+    const latestData = await parse(await this.getLatestStatus())
+    const existingDataSource = await this.getExistingStatus()
+    const existingData = {}
+    const updateQueue = []
+    const insertionQueue = []
+
+    for (let i = 0; i < existingDataSource.length; i++) {
+      delete existingDataSource[i].id
+
+      existingData[Number(existingDataSource[i].identify)] = existingDataSource[i]
+    }
+
+    for (let i = 0; i < latestData.length; i++) {
+      const data = {
+        identify: latestData[i]['연번'] || '',
+        province: latestData[i]['시도'] || '',
+        city: latestData[i]['시군구'] || '',
+        clinicName: latestData[i]['기관명'] || '',
+        address: latestData[i]['주소'] || '',
+        requestType: latestData[i]['신청유형'] || '',
+        representativeContact: latestData[i]['전화번호'] || '',
+        availableAt: new Date(latestData[i]['운영가능일자'].split('.').slice(0, 3).join('-') || 0)
+      }
+      const dataKeys = Object.keys(data)
+
+      if (!data.identify) continue
+      if (!existingData[Number(data.identify)]) {
+        insertionQueue.push(data)
+
+        continue
+      }
+
+      for (let k = 0; k < dataKeys.length; k++) {
+        if (data[dataKeys[k]] !== existingData[Number(data.identify)][dataKeys[k]]) {
+          updateQueue.push(data)
+
+          continue
+        }
+      }
+    }
+
+    for (let i = 0; i < insertionQueue.length; i++) {
+      await knex('SafelySeparatedClinics')
+        .insert(insertionQueue[i])
+    }
+    for (let i = 0; i < updateQueue.length; i++) {
+      await knex('SafelySeparatedClinics')
+        .update(updateQueue[i])
+        .where({
+          identify: updateQueue[i].identify
+        })
+    }
+
     this.debug('updated the situation report')
+
+    setTimeout(() => {
+      this.debug('scheduled the update at ' + Date.now() + this.updateAfter)
+
+      this.update()
+    }, this.updateAfter)
   }
 }
 
